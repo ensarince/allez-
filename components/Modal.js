a891feab68d9576883fa61e665f4666d3d027e01
ensarince <32306029+ensarince@users.noreply.github.com>
2023-01-05 22:22:58 +0100

    some responsive design corrections1

--- components/Modal.js
+++ components/Modal.js
@@ -94,7 +94,7 @@ function Modal() {
         <Dialog as='div' 
                 className="fixed z-30 inset-0 overflow-y-auto"
                 onClose={setOpen}>
-        <div className='flex items-end justify-center min-h-800 sm:min-h-screen pt-4 px-4 pb-20 text-center
+        <div className='flex items-end justify-center min-h-screen md:h-1200 pt-4 px-4 pb-20 text-center
              sm:block sm:p-0'>
                 <Transition.Child
                     as={Fragment}
