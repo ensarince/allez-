ce13978b5561dd67865d037a90c0c14759ff8dfe
ensarince <32306029+ensarince@users.noreply.github.com>
2023-01-17 14:48:15 +0100

    added redis/upstash messenger functionality with pusher1

--- pusher.ts
+++ pusher.ts
@@ -4,7 +4,7 @@ import ClientPusher from "pusher-js"
 export const serverPusher = new Pusher({
     appId: "1537954",
     key: "98ce91e45153d6dd8f9c",
-    secret: process.env.PUSHER_SECRET,
+    secret: process.env.PUSHER_SECRET!,
     cluster: "eu",
     useTLS: true
 })
