ddf1bf6c9a39c2ff3e1281711ea9b694aa3b7e54
ensarince <32306029+ensarince@users.noreply.github.com>
2023-01-18 10:39:45 +0100

    added redis/upstash messenger functionality with pusher3

--- components/MessageComponent.js
+++ components/MessageComponent.js
@@ -17,7 +17,7 @@ function MessageComponent({message}) {
             <p className={`text-[0.65rem] px-[2px] pb-[2px] ${isUser ? "text-blue-400 text-right" : "text-red-400 text-left"}`}>{message.username}</p>
 
             <div className="flex items-end">
-                <div className={`px-3 py-2 rounded-lg w-fit text-white ${isUser ? "bg-blue-400 ml-auto order-2" : "bg-red-400 "}`}>
+                <div className={`px-3 py-2 rounded-lg w-fit break-all text-white ${isUser ? "bg-blue-400 ml-auto order-2" : "bg-red-400 "}`}>
                     <p>{message.message}</p>
                 </div>
 
